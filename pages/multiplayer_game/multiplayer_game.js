40804cbcc1ac41dd1871208b1d064a27a5c537dc
ZhuYacheng <3059742980@qq.com>
2021-10-10 00:57:33 +0800

    多人博饼

--- pages/multiplayer_game/multiplayer_game.js
+++ pages/multiplayer_game/multiplayer_game.js
@@ -1,65 +1,61 @@
-// pages/multiplayer_game/multiplayer_game.js
+// pages/solitaire_game/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    num1:0,
+    num2:0, 
+    num3:0,
+    num4:0, 
+    num5:0,
+    num6:0,
+    arr:[],
+    userNumbers : 0,
+    now_num:0,
+    rank_list:[
+              "一秀",
+              "二举",
+              "四进",
+              "三红",
+              "对堂",
+              "状元!",
+              "五子登科!",
+              "五子带一秀!",
+              "五王!",
+              "六杯黑!",
+              "六杯红!",
+              "状元插金花!"
+    ]
+  },                                           
+  handle_dice(e){
+    var app = getApp()
+    var temp = [0,0,0,0,0,0,0]
+    this.setData({
+          num1: Math.ceil(Math.random()*6), 
+          num2: Math.ceil(Math.random()*6), 
+          num3: Math.ceil(Math.random()*6), 
+          num4: Math.ceil(Math.random()*6),  
+          num5: Math.ceil(Math.random()*6), 
+          num6: Math.ceil(Math.random()*6), 
+          userNumbers: app.globalData.all_num,
+    });
+    temp[Number(this.data.num1)] += 1;
+    temp[Number(this.data.num2)] += 1;
+    temp[Number(this.data.num3)] += 1;
+    temp[Number(this.data.num4)] += 1;
+    temp[Number(this.data.num5)] += 1;
+    temp[Number(this.data.num6)] += 1;
+    
+    this.setData({
+          arr:temp,
+          now_num:Number(this.data.now_num)+1
+    })   
+    console.log(this.data.now_num)
+    console.log(this.data.userNumbers)
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
 
-  },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
